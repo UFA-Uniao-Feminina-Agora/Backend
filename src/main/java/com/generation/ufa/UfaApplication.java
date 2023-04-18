4442c2e363000906bff102f93e069a9632a63568
deizirrs <deizianesantos.rodrigues15@gmail.com>
2023-04-18 10:20:49 -0300

    Criação do projeto spring

---
+++ src/main/java/com/generation/ufa/UfaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.ufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/ufa/UfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.ufa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
