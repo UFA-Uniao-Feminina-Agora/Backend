118bb172b47f76c02cb2e20bbb6e5f3131dec2ce
r4inha <kadu_300live.com>
2023-04-24 09:47:26 -0300

    Task 4 TemaRepository

---
+++ src/main/java/com/generation/ufa/repository/TemaRepository.java
@@ -0,0 +1,14 @@
+package com.generation.ufa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.ufa.model.Tema;
+
+public interface TemaRepository extends JpaRepository <Tema, Long> {
+	List <Tema>findAllByNome_temaContainingIgnoreCase (@Param ("nome_tema") String nome_tema);
+	
+	
+}
