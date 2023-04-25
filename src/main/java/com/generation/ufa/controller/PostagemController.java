0ecc8fe28a1960cfcf07061336cb55a288a058bb
Carolina Correa <carolina.unila@gmail.com>
2023-04-25 16:12:33 -0300

    ufa

--- src/main/java/com/generation/ufa/controller/PostagemController.java
+++ src/main/java/com/generation/ufa/controller/PostagemController.java
@@ -1,7 +1,8 @@
 package com.generation.ufa.controller;
 
-import java.util.Date;
+//import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,12 +16,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.ufa.model.Postagem;
 import com.generation.ufa.repository.PostagemRepository;
 
 import jakarta.validation.Valid;
-
+//import jakarta.validation.constraints.PastOrPresent;
 
 @RestController
 @RequestMapping("/postagens")
@@ -41,10 +43,12 @@ public class PostagemController {
 	}
 	
 	@PutMapping("/{id}") 
-	public ResponseEntity<Postagem>put(@Valid @RequestBody Postagem postagem) {
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta->ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());	
+	public void put (@PathVariable Long id, @Valid @RequestBody Postagem postagem) {
+		Optional <Postagem>post = postagemRepository.findById(id);
+		if(post.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+		
 	}
 	
 	@DeleteMapping("/{id}")
@@ -72,10 +76,9 @@ public class PostagemController {
 	}
 	
 	
-	/*@GetMapping("/data/{data}")
-	public List<Postagem>findByData(){
-		return postagemRepository.findByData(new Date());
-		*/
-	
+	/*@GetMapping("/{data}")
+	public ResponseEntity <List<Postagem>> getByData (@PastOrPresent LocalDateTime data){
+		return ResponseEntity.ok(postagemRepository.findAllByData(data));
+	}*/
 	
 }
