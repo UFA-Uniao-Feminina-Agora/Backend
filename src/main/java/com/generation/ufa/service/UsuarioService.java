e28db81758c7404bd2c03c278dc6b362b2822c60
Michelle Greghi <michelle.neves23@gmail.com>
2023-04-28 14:06:44 -0300

    Atualização

--- src/main/java/com/generation/ufa/service/UsuarioService.java
+++ src/main/java/com/generation/ufa/service/UsuarioService.java
@@ -2,11 +2,13 @@ package com.generation.ufa.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.ufa.model.Usuario;
 import com.generation.ufa.model.UsuarioLogin;
@@ -38,13 +40,17 @@ public class UsuarioService {
 	
 	public Optional<Usuario> atualizarUsuario(Usuario usuario){
 		
-		if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) {
+		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
+			
+			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+			
+			if((buscaUsuario.isPresent()) && (buscaUsuario.get().getId() != usuario.getId()))
+					throw new ResponseStatusException (HttpStatus.BAD_REQUEST, "Usuário já existe!" , null);
+					
+		usuario.setSenha(criptografarSenha(usuario.getSenha()));
 		
-			usuario.setSenha(criptografarSenha(usuario.getSenha()));
-			
-			return Optional.ofNullable(usuarioRepository.save(usuario));
-			
-		}
+		return Optional.ofNullable(usuarioRepository.save(usuario));
+	}
 		
 		return Optional.empty();
 	}
